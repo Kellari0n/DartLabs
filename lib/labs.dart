9ad1d5dd623976c558dc471941645f34869edf57
Георгий Филиппов <filippovgg2@gmail.com>
2024-12-25 13:47:16 +0300

    Add LabManager

---
+++ lib/labs.dart
@@ -0,0 +1,98 @@
+class LabManager {
+  final List<Lab> _labs = [];
+
+  void addLab(Lab lab) {
+    _labs.add(lab);
+  }
+
+  void start([int? labNum, int? partNum, int? exNum]) {
+    if (labNum == null) {
+      for (var lab in _labs) {
+        lab.startLab();
+      }
+    }
+    else {
+      if (partNum == null) {
+        _labs.firstWhere((p) => p._number == partNum).startLab();
+      }
+      else {
+        if (exNum == null) {
+          _labs.firstWhere((p) => p._number == partNum).startLab(partNum);
+        }
+        else {
+          _labs.firstWhere((p) => p._number == partNum).startLab(partNum, exNum);
+        }
+      }
+    }
+  }
+}
+
+class Lab {
+	final int _number;
+	final String _name;
+	final List<LabPart> _parts;
+	
+	Lab(this._number, this._name, this._parts);
+
+  void startLab([int? partNum, int? exNum]) {
+    print("Lab $_number. $_name:");
+    if (partNum == null) {
+      for (var part in _parts) {
+        part.startPart();
+      }
+    }
+    else{
+      if (exNum == null) {
+        _parts.firstWhere((p) => p._number == partNum).startPart();
+      }
+      else {
+        _parts.firstWhere((p) => p._number == partNum).startPart(exNum);
+      }
+    }
+  }
+}
+
+class LabPart {
+	final int _number;
+	final String _name;
+	final List<Exercise> _exercises;
+
+	LabPart(this._number, this._name, this._exercises);
+
+  void startPart([int? exNum]) {
+    print("Part $_number. $_name:");
+    if (exNum == null) {
+      for (var ex in _exercises) {
+        ex.startExercise();
+      }
+    }
+    else {
+      _exercises.firstWhere((ex) => ex._number == exNum).startExercise();
+    }
+  }
+}
+
+class Exercise {  
+	final int _number;
+	final String _exercise;
+	final Function? _solution;
+
+	Exercise(this._number, this._exercise, this._solution);
+
+  void startExercise() {
+    print("Exercise $_number:");
+    print(_exercise);
+    if (_solution != null) {
+      _solution();
+    }
+    else {
+      print("Exercise has no solution");
+    }
+  }
+}
+
+LabManager InitManager() {
+  var labManager = LabManager();
+    
+  return labManager;
+}
